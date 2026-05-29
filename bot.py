1f7f7abedf0886cfa916859b26d351a567734324
solihovnahtullo-crypto <solihovnahtullo@gmail.com>
2026-05-29 21:16:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,5 +39,5 @@ def main():
  app.add_handler(CommandHandler("start",start))
  app.add_handler(MessageHandler(filters.TEXT&~filters.COMMAND,handle))
  logger.info("Bot started!")
- app.run_polling()
+ app.run_polling(allowed_updates=Update.ALL_TYPES)
 if __name__=="__main__":main()
